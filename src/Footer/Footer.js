6df985ea29c8738994b0b2b4a09deacba4ee75b9
luuds <luuds@luuds.myguest.virtualbox.org>
2024-01-26 15:48:21 -0300

    react spotify alura

---
+++ src/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+        <footer class="disclaimer_premium">
+        <div class="text">
+            <p class="disclaimer_premium_title">Testar o Premium de graça</p>
+            <p class="disclaimer_premium_subtitle">
+            Inscreva-se para curtir música ilimitada e podcasts só com alguns
+            anúncios. Não precisa de cartão de crédito.
+            </p>
+        </div>
+        <div class="button">
+            <button type="button">Inscreva-se grátis</button>
+        </div>
+    </footer>
+    );
+  };
+  
+  export default Footer;
